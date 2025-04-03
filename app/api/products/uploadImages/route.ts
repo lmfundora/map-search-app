8d2924707d2536a2f3997112e8fa5330818a5700
lmfundora <lmfundora2@gmail.com>
2025-04-03 13:38:23 -0400

    Add image upload routes for locales and products

---
+++ app/api/products/uploadImages/route.ts
@@ -0,0 +1,29 @@
+import { prisma } from "@/lib/prisma_client";
+import { formatResponse } from "../../formatResponse";
+
+export async function GET(request: Request) {
+  const locales = await prisma.productos.findMany();
+  const fs = require("fs");
+  const path = require("path");
+
+  const directoryPath = "/home/david/Downloads/products";
+  const files = fs.readdirSync(directoryPath);
+
+  const fileNames = files.map((file: any) => {
+    return path.parse(file).name + path.parse(file).ext;
+  });
+
+  locales.forEach(async (locale, index) => {
+    console.log(locale.id, fileNames[index]);
+    await prisma.locales.update({
+      where: { id: locale.id },
+      data: { image: fileNames[index] },
+    });
+  });
+
+  const { data, headers } = formatResponse({
+    message: "Unimplemented",
+  });
+
+  return new Response(data, headers);
+}
